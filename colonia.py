e032f5866a1ce7618db4b4f86c80fbda338d819e
Nixprow <nvaldivia470@gmail.com>
2025-06-24 22:31:42 -0400

    Se comienza desarrollo de Clase Colonia y sus funciones como paso, que vendria siendo el metodo que desarrolla todo

--- colonia.py
+++ colonia.py
@@ -1,2 +1,34 @@
+from bacteria import Bacteria
+from ambiente import Ambiente
+import random
+
 class Colonia():
-    pass
+    def __init__(self,ambiente):
+        self.__bacterias= [] #Lista de objetos
+        self.__ambiente= None #Considera la instancia de ambiente
+        self.__posiciones_bacterias= {}
+    def get_ambiente(self):
+        return self.__ambiente
+    
+    def set_ambiente(self,ambiente):
+        if isinstance(ambiente, Ambiente):
+            self.__ambiente= ambiente
+        else: 
+            raise TypeError("El tipo no corresponde a la instancia Ambiente")
+        
+    
+    def agregar_bacteria(self,bacteria=Bacteria(),ambiente= Ambiente(), x=None, y=None):
+        if x is None or y is None:
+            x=random.randint(0,ambiente.get_dimensiones()[0]- 1)
+            y=random.randint(0,ambiente.get_dimensiones()[1]- 1)
+        if not self.celda_ocupada(x,y):
+            self.__bacterias.append(bacteria)
+            self.__posiciones_bacterias[bacteria.get_id()] =(x,y)
+            
+    def celda_ocupada(self,x,y):
+        return (x,y) in self.__posiciones_bacterias.values()
+    
+    def paso(self):
+        nuevas_bacterias= [] #Variable para almacenar las bacterias nuevas
+            
+            
