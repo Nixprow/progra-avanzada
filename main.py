8d83262100cedc3aa03e075ab9d0d38f2b8b6689
Nixprow <nvaldivia470@gmail.com>
2025-06-25 09:19:30 -0400

    Se añade boton de siguiente paso

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from gi.repository import Gtk, Gio
 
 class MainSimulador(Gtk.Application):
     def __init__(self):
-        super().__init__(application_id="org.proyecto_final.utal")
+        super().__init__(application_id="org.simulador.ColoniaUI")
         self.connect("activate", self.on_activate)
 
     def on_activate(self, app):
@@ -15,12 +15,12 @@ class MainSimulador(Gtk.Application):
         self.ventana.set_title("Simulador de Colonia Bacteriana")
         self.ventana.set_default_size(600, 400)
 
-        
+        # HeaderBar
         header = Gtk.HeaderBar()
         header.set_title_widget(Gtk.Label(label="Simulador Bacteriano"))
         header.set_show_title_buttons(True)
 
-    
+        #Aqui se plantea la lista desplegable de opciones
         menu = Gio.Menu()
         menu.append("Importar CSV", "app.importar")
         menu.append("Graficar Resultados", "app.graficar")
@@ -31,7 +31,7 @@ class MainSimulador(Gtk.Application):
 
         self.ventana.set_titlebar(header)
 
-        
+        # Acciones del menú 
         importar_action = Gio.SimpleAction.new("importar", None)
         importar_action.connect("activate", self.on_importar)
         self.add_action(importar_action)
@@ -40,9 +40,19 @@ class MainSimulador(Gtk.Application):
         graficar_action.connect("activate", self.on_graficar)
         self.add_action(graficar_action)
 
-    
-        etiqueta = Gtk.Label(label="Aquí se mostrará la simulación de la colonia.")
-        self.ventana.set_child(etiqueta)
+       
+        caja_principal = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10, margin_top=10, margin_bottom=10, margin_start=10, margin_end=10)
+
+        self.etiqueta_estado = Gtk.Label(label="Aquí se mostrará la simulación de la colonia.")
+        caja_principal.append(self.etiqueta_estado)
+
+        boton_siguiente = Gtk.Button(label="Siguiente Paso")
+        boton_siguiente.set_halign(Gtk.Align.END)
+        boton_siguiente.set_valign(Gtk.Align.END)
+        boton_siguiente.connect("clicked", self.on_siguiente_paso)
+        caja_principal.append(boton_siguiente)
+
+        self.ventana.set_child(caja_principal)
         self.ventana.present()
 
     def on_importar(self, action, param):
@@ -50,6 +60,10 @@ class MainSimulador(Gtk.Application):
 
     def on_graficar(self, action, param):
         print("[MENU] Graficar resultados seleccionado")
+
+    def on_siguiente_paso(self, button):
+        print("[BOTON] Siguiente paso de simulación ejecutado")
+        self.etiqueta_estado.set_text("Simulación: siguiente paso ejecutado")
 
 if __name__ == "__main__":
     app = MainSimulador()
